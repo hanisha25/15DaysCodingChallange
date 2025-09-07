7d004370cd3fd270f191dc0cee7f3d25e7832f5d
Hanisha <167543430+hanisha25@users.noreply.github.com>
2025-09-07 14:11:21 +0530

    Time: 1 ms (82.98%) | Memory: 42.3 MB (13.00%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,27 +1,20 @@
 class Solution {
     int[][] dp;
+
     public int maxProfit(int[] prices) {
-        dp=new int[prices.length][2];
-        for(int[] d:dp)
-        {
-            Arrays.fill(d,-1);
+        int n=prices.length;
+        dp = new int[prices.length + 2][2];
+        for (int idx = n-1; idx >= 0; idx--) {
+            for (int buy = 0; buy < 2; buy++) {
+                if (buy == 1) {
+                    dp[idx][1] = Math.max(dp[idx + 1][1],
+                            -prices[idx] + dp[idx + 1][0]);
+                } else {
+                     dp[idx][0] = Math.max(dp[idx + 1][0],
+                            prices[idx] + dp[idx + 2][1]);
+                }
+            }
         }
-        return recursion(prices,0,1);
-    }
-    public int recursion(int[] prices,int idx,int buy)
-    {
-        if(idx>=prices.length)
-        {
-            return 0;
-        }
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
-        if(buy==1 )
-        {
-            return dp[idx][1]=Math.max(recursion(prices,idx+1,1),-prices[idx]+recursion(prices,idx+1,0));
-        }
-        else
-        {
-            return dp[idx][0]=Math.max(recursion(prices,idx+1,0),prices[idx]+recursion(prices,idx+2,1));
-        }
+        return dp[0][1];
     }
 }
