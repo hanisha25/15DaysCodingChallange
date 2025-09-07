69461fbcd8120d3d988a5685a00c79fab74862db
Hanisha <167543430+hanisha25@users.noreply.github.com>
2025-09-07 14:25:53 +0530

    Time: 16 ms (58.36%) | Memory: 55 MB (81.95%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,29 +1,34 @@
 class Solution {
-    int[][] dp;
-    int fees;
     public int maxProfit(int[] prices, int fee) {
-        fees=fee;
-        dp=new int[prices.length][2];
-        for(int[] d:dp)
-        {
-            Arrays.fill(d,-1);
+        int n=prices.length;
+        int[][] dp = new int[prices.length + 1][2];
+        for (int idx = n-1; idx >= 0; idx--) {
+            for (int buy = 0; buy < 2; buy++) {
+                if (buy == 1) {
+                    dp[idx][1] = Math.max(dp[idx + 1][1],
+                            -(prices[idx]+fee) + dp[idx + 1][0]);
+                } else {
+                     dp[idx][0] = Math.max(dp[idx + 1][0],
+                            prices[idx] + dp[idx + 1][1]);
+                }
+            }
         }
-        return recursion(prices,0,1);
+        return dp[0][1];
     }
-    public int recursion(int[] prices,int idx,int buy)
-    {
-        if(idx>=prices.length)
-        {
-            return 0;
-        }
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
-        if(buy==1 )
-        {
-            return dp[idx][1]=Math.max(recursion(prices,idx+1,1),-(prices[idx]+fees)+recursion(prices,idx+1,0));
-        }
-        else
-        {
-            return dp[idx][0]=Math.max(recursion(prices,idx+1,0),prices[idx]+recursion(prices,idx,1));
-        }
-    }
+    // public int recursion(int[] prices,int idx,int buy)
+    // {
+    //     if(idx>=prices.length)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[idx][buy]!=-1) return dp[idx][buy];
+    //     if(buy==1 )
+    //     {
+    //         return dp[idx][1]=Math.max(recursion(prices,idx+1,1),-(prices[idx]+fees)+recursion(prices,idx+1,0));
+    //     }
+    //     else
+    //     {
+    //         return dp[idx][0]=Math.max(recursion(prices,idx+1,0),prices[idx]+recursion(prices,idx,1));
+    //     }
+    // }
 }
