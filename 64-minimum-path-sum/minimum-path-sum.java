cdf13a9a3c6c05e08ba2573a99498ca1f97efaf1
Hanisha <167543430+hanisha25@users.noreply.github.com>
2025-09-17 21:34:56 +0530

    Time: 3 ms (69.97%) | Memory: 47.4 MB (90.04%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,22 +1,27 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-       int n=grid.length,m=grid[0].length;
-       int[][] dp=new int[n][m];
-       int sum=0;
-       for(int i=0;i<m;i++){
-           sum+=grid[0][i];
-           dp[0][i]=sum;
-       }
-       sum=0;
-       for(int i=0;i<n;i++){
+        int n=grid.length;
+        int m=grid[0].length;
+        int[][] dp=new int[n][m];
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
            sum+=grid[i][0];
            dp[i][0]=sum;
-       }
-       for(int i=1;i<n;i++){
-        for(int j=1;j<m;j++){
-            dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
         }
-       }
-       return dp[n-1][m-1];
+        sum=0;
+        for(int j=0;j<m;j++)
+        {
+            sum+=grid[0][j];
+           dp[0][j]=sum;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                dp[i][j]=grid[i][j]+Math.min(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        return dp[n-1][m-1];
     }
 }
