058549bd9b5f5fb73668fec291191b3ec7141749
Hanisha <167543430+hanisha25@users.noreply.github.com>
2025-09-30 23:17:27 +0530

    Time: 2 ms (99.91%) | Memory: 43.3 MB (73.33%) - LeetSync

---
+++ 1547-destination-city/destination-city.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        HashSet<String> set=new HashSet<>();
+        for(List<String> list:paths)
+        {
+            set.add(list.get(0));
+        }
+        for(List<String> list:paths)
+        {
+            if(!set.contains(list.get(1)))
+            {
+                return list.get(1);
+            }
+        }
+        return "";
+    }
+}
