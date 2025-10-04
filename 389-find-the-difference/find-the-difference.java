fc7032115363b8101f455e4c550ca24540e873e9
Hanisha <167543430+hanisha25@users.noreply.github.com>
2025-10-04 23:44:47 +0530

    Time: 1 ms (99.31%) | Memory: 42.3 MB (10.51%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int res=0;
+        for(char ch:t.toCharArray())
+        {
+            res+=ch;
+        }
+        for(char ch:s.toCharArray())
+        {
+            res-=ch;
+        }
+        return (char)res;
+    }
+}
