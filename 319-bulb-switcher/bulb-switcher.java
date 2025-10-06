4247f92a9921289ae6633eea95c89e845a3d0ced
Hanisha <167543430+hanisha25@users.noreply.github.com>
2025-10-06 12:16:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (37.90%) - LeetSync

---
+++ 319-bulb-switcher/bulb-switcher.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        // int bulbs = 0;
+        // for (int i = 1; i <= n; i++) {
+        //     if ((i * i) <= n) {
+        //        bulbs++;
+        //     }
+        // }
+        // return bulbs;
+        return (int)Math.sqrt(n);
+    }
+}
