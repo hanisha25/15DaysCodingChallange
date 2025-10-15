4600ee9e7d8c209f940f657e291c95033ca42ee1
Hanisha <167543430+hanisha25@users.noreply.github.com>
2025-10-15 18:40:33 +0530

    Time: 1 ms (80.03%) | Memory: 41.5 MB (49.94%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+       HashMap<Character, String> hm = new HashMap<>();
+        String arr[] = s.split(" ");
+        if (pattern.length() != arr.length) return false;
+        for(int i=0;i<pattern.length();i++)
+        {
+            char ch=pattern.charAt(i);
+            if(hm.containsKey(ch))
+            {
+                if(!hm.get(ch).equals(arr[i]))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if(hm.containsValue(arr[i]))
+                {
+                    return false;
+                }
+                hm.put(ch,arr[i]);
+            }
+        }
+        return true;
+    }
+}
